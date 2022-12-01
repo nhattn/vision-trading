234864ad734768b999f21b0096457d0ad6b92c94
Trần Ngọc Nhật <anhchangthode22145@gmail.com>
2022-12-02 00:05:09 +0700

    Default precision of 6 digits and we cannot change it. If we want customised precision then you should use stringstream. Let's set precision to 2
    Escape '"' for name + add timestamp to string export in `to_string`
    function member

--- order.cpp
+++ order.cpp
@@ -1,6 +1,29 @@
 #include <order.h>
+#include <iomanip>
+#include <sstream>
 
 namespace vision {
+    namespace {
+        inline std::string precision_2_digit(double num) {
+            std::ostringstream ss;
+            ss << std::fixed;
+            ss << std::setprecision(2);
+            ss << num;
+            return ss.str();
+        }
+        inline std::string escape_name(std::string s) {
+            std::string res;
+            for(unsigned int i = 0; i < s.length(); i++) {
+                if (s.at(i) == '"') {
+                    if (i == 0 || s.at(i - 1) != '\\') {
+                        res.append(1, '\\');
+                    }
+                }
+                res.append(1, s.at(i));
+            }
+            return res;
+        }
+    }
     Order::Order(int id, std::string name, double price, double quantity,
            int time, OrderAction action, OrderType type) : ID(id),
         Name(name), Price(price), Quantity(quantity), Time(time),
@@ -20,9 +43,10 @@ namespace vision {
         return !operator == (rhs);
     }
     std::string Order::to_string() const {
-        return "{\"id\":" + std::to_string(ID) + ",\"name\":\"" + Name + "\""
-               ",\"price\":" + std::to_string(Price) + ",\"quantity\":" + std::to_string(Quantity) + ","
-               "\"action\":\"" + vision::to_string(Action) + "\",\"type\":\"" + vision::to_string(Type) + "\"}";
+        return "{\"id\":" + std::to_string(ID) + ",\"name\":\"" + escape_name(Name) + "\""
+               ",\"price\":" + precision_2_digit(Price) + ",\"quantity\":" + precision_2_digit(Quantity) + ","
+               "\"action\":\"" + vision::to_string(Action) + "\",\"type\":\"" + vision::to_string(Type) + "\""
+               ",\"timestamp\":" + std::to_string(Time) + "}";
     }
     const std::string to_string(OrderAction action) {
         if(action == OrderAction::SELL) return std::string("Sell");
--- vision.cpp
+++ vision.cpp
@@ -4,5 +4,7 @@
 int main(int argc, char **argv) {
     vision::Order ord(1, "RMF23", 1892, 2, static_cast<int>(time(nullptr)), vision::OrderAction::BUY, vision::OrderType::LIMIT);
     std::cout << ord.to_string() << std::endl;
+    vision::Order ord1(2, "RX\"03", 1896, 1,  static_cast<int>(time(nullptr)));
+    std::cout << ord1.to_string() << std::endl;
     return 0;
 }
